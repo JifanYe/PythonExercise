c20c375262d68d6ed1ebd53d361e0fe9d3b87cc5
Ryan Ye <jifanye@outlook.com>
2019-09-16 17:18:50 +0800

    20190916

--- python_basics.py
+++ python_basics.py
@@ -1,3 +1,4 @@
+'''
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
@@ -16,6 +17,21 @@ for key in d:
 e = [x * x for x in range(1, 11)]
 for i in e:
     print(i)
+'''
 
-
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
     
+    return fs
+temp = 0
+#f1 = count()
+f1, f2, f3 = count()
+a1, a2, a3 = [1, 3, 5]
+
+print(f1)
+print(f2)
+print(f3)
